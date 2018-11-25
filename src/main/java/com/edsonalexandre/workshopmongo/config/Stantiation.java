34e8a3e2b92f24277eab4f0688c0548329cb4b71
Edson Alexandre <edson.alexandre@outlook.com>
2018-11-25 11:41:10 -0200

    Operação de instanciação da base de dados

---
+++ src/main/java/com/edsonalexandre/workshopmongo/config/Stantiation.java
@@ -0,0 +1,30 @@
+package com.edsonalexandre.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.edsonalexandre.workshopmongo.domain.User;
+import com.edsonalexandre.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Stantiation implements CommandLineRunner {
+	
+	@Autowired 
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+	}
+
+}
