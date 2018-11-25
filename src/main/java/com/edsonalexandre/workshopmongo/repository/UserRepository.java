32684f6bfe2a7478916df754752fd15d5c40f8a0
Edson Alexandre <edson.alexandre@outlook.com>
2018-11-25 10:44:01 -0200

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/edsonalexandre/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.edsonalexandre.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edsonalexandre.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
