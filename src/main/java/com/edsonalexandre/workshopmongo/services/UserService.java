7983d7dfeb847139dd53ea6bb80a608e4e127570
Edson Alexandre <edson.alexandre@outlook.com>
2018-11-27 20:22:38 -0200

    Inserção de usuário com post

--- src/main/java/com/edsonalexandre/workshopmongo/services/UserService.java
+++ src/main/java/com/edsonalexandre/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edsonalexandre.workshopmongo.domain.User;
+import com.edsonalexandre.workshopmongo.dto.UserDTO;
 import com.edsonalexandre.workshopmongo.repository.UserRepository;
 import com.edsonalexandre.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDto(UserDTO userDto) {		
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 }
