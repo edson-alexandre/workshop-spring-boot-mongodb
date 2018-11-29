ee971baa3b61542077eb10801623feec18d2b012
Edson Alexandre <edson.alexandre@outlook.com>
2018-11-29 21:01:15 -0200

    Obtendo o post por id

---
+++ src/main/java/com/edsonalexandre/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.edsonalexandre.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edsonalexandre.workshopmongo.domain.Post;
+import com.edsonalexandre.workshopmongo.domain.User;
+import com.edsonalexandre.workshopmongo.repository.PostRepository;
+import com.edsonalexandre.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+	}
